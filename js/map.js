65ab7857296f9a7fbeaa5697f59062d45661c9ff
gracelizzie <gracelizzibutcher@gmail.com>
2026-09-14 12:32:35 +0100

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,76 @@
+function initialiseMap() {
+
+    console.log("Initialising map...");
+
+
+    const baseVectors =
+        document.querySelectorAll("#base-vectors > *");
+
+
+    const treeVectors =
+        document.querySelectorAll("#tree-vectors > *");
+
+
+    console.log(
+        `Found ${baseVectors.length} base objects`
+    );
+
+
+    console.log(
+        `Found ${treeVectors.length} tree objects`
+    );
+
+
+    addClickListeners(baseVectors);
+
+    addClickListeners(treeVectors);
+
+}
+
+
+
+function addClickListeners(features) {
+
+
+    features.forEach(feature => {
+
+
+        console.log(feature.id);
+
+
+        feature.style.cursor = "pointer";
+        feature.style.pointerEvents = "all";
+
+
+        feature.addEventListener("click", () => {
+
+
+            openPopup(feature.id);
+
+
+        });
+
+
+    });
+
+
+}
+
+
+function enableZoom() {
+
+    const svg = document.querySelector("#map svg");
+
+    const panzoom = Panzoom(svg, {
+        maxScale: 5,
+        minScale: 0.5,
+        contain: "outside"
+    });
+
+
+    svg.parentElement.addEventListener(
+        "wheel",
+        panzoom.zoomWithWheel
+    );
+
+}
